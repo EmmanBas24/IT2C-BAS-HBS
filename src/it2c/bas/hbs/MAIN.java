06b9ea6c2b4e6d0243eb9792f49a15b791fba591
AdolfXTR <adolfborja132@gmail.com>
2024-10-09 14:57:14 +0800

    update delete

--- src/it2c/bas/hbs/MAIN.java
+++ src/it2c/bas/hbs/MAIN.java
@@ -27,14 +27,54 @@ public class MAIN {
 }
 
 
+  private void viewGuest() {
+        String cqry = "SELECT * FROM Guest";
+        String[] Headers = {"ID", "First Name", "Last Name", "Contact Number", "Email"};
+        String[] Columns = {"g_id", "g_fname", "g_lname", "g_cnum", "g_email"};
+        config conf = new config();
+        conf.viewRecords(cqry, Headers, Columns);
+    }
+  
+  private void updateGuest(){
+  
+      Scanner sc = new Scanner(System.in);
+        System.out.print("Enter guest ID ");
+        int id = sc.nextInt();
+  
+         System.out.print("Enter the new first name: ");
+         String ufname = sc.next();
+         System.out.print("Enter the new last name: ");
+         String ulname = sc.next();
+         System.out.print("Enter the new contact number: ");
+         String ucnum = sc.next();
+         System.out.print("Enter the new Email: ");
+         String uemail = sc.next();
+         
+         String qry = "UPDATE Guest SET g_fname = ?, g_lname = ?, g_cnum = ? , g_email = ? WHERE g_id = ?";
+         config conf = new config();
+         conf.updateRecord(qry, ufname, ulname, ucnum, uemail, id);
+  }
+ 
+  private void deleteGuest(){
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter guest ID to delete: ");
+         int id = sc.nextInt();
+
+         String sqlDelete = "DELETE FROM Guest WHERE g_id = ?";
+         config conf = new config();
+         conf.deleteRecord(sqlDelete, id);
+  }
+  
+  
+  
 public static void main(String[] args) {
   
-         connectDB();
-         Scanner sc = new Scanner(System.in);
-         String op = null;
-  
+        connectDB();
+        Scanner sc = new Scanner(System.in);
+        String op = null;
+        MAIN guest = new MAIN();
+     
         do{
-
         System.out.println("WELCOME TO BOOKIING SYSTEM");
         System.out.println("-------------------------------------");
         System.out.println("1. ADD");
@@ -54,14 +94,31 @@ public static void main(String[] args) {
         } switch(option){
 
             case 1:
-                 MAIN demo = new MAIN();
-                 demo.addGuest();
+                 guest.addGuest();
             break;
-    }     System.out.println("Do you want to continue?(Y/N): ");
+          
+            case 2:
+                 guest.viewGuest();
+            break;
+          
+            case 3:
+                guest.viewGuest();
+                guest.updateGuest();
+            break;
+           
+            case 4:
+                guest.viewGuest();
+                guest.deleteGuest();
+            break;
+            
+            case 5:
+                System.exit(0);
+                break;
+            
+    }     System.out.print("Do you want to continue?(Y/N): ");
             op=sc.next();
         }while(op.equals("Y")|| op.equals("y"));
-
+         System.out.println("THANK YOU MY LOVE");
     }
 }
-    
     
